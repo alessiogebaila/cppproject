303d8b9f0f8c128690330fbb5cdf2a6684d0c169
Alessio Gebaila <gebailaalessio22@stud.ase.ro>
2023-11-25 20:16:48 +0200

    event class

--- myproject/Source.cpp
+++ myproject/Source.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+#include <iostream>;
+#include <string>;
 
 
-int main() {
-	std::cout << std::endl << "Hello world!";
-}
+class Event
+{
+private:
+	const int event_id;
+	static int event_nr;
+	char* event_name;
+	int tickets_available;
+	double ticket_prices;
+
+public:
+	Event();
+	Event(char* event_name, int my_tickets, double ticket_prices);
+	Event(const Event& my_event);
+	~Event();
+	Event& operator=(const Event& my_event);
+
+	char* get_event_name();
+	int get_tickets_available();
+	double get_ticket_prices();
+	void set_event_name(char* new_event_name);
+	void set_tickets_available(int new_tickets_available);
+	void set_ticket_prices(double new_ticket_prices);
+	static int get_events_number();
+	static void set_events_number(unsigned new_events_number);
+
+	double BuyTicket(int new_tickets_available);
+};
